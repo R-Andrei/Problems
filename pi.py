d89d94e48c076677307ad4e552c1f6dfdd0d8895
R-Andrei <faragau.rares@gmail.com>
2020-03-15 01:24:48 +0200

    initial commit

---
+++ pi.py
@@ -0,0 +1,13 @@
+from random import uniform as rand
+
+# Fuction rand generates numbers between 0 and 1. The distribution of generated numbers is uniform.
+# Using the rand function, approximate the value of pi.
+
+def calculate_pi(magnitude=5):
+    inside_area = 0
+    for _ in range(10**magnitude):
+        inside_area += 1 if rand(0, 1)**2 + rand(0, 1)**2 <= 1 else 0
+    return 4 * inside_area / 10 ** magnitude
+
+pi =calculate_pi(magnitude=7)
+print(pi)
